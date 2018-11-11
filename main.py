a2e5d410e123d66cf6f88e1054b4e1d921859e25
Robert Adragna <robert.adragna1381@gmail.com>
2018-11-11 00:46:23 -0500

    Added graphing, loging and evaluation to training loop. Note that this is not fully debugged

--- main.py
+++ main.py
@@ -5,17 +5,32 @@ import numpy as np
 from dataset import SongDataset
 from model import ConvClassifier
 
+import matplotlib.pyplot as plt
+import scipy.signal as sig
+
+def plot_accuracy_vs_stepnum(step_list, data_list, label, sig_fact_1, sig_fact_2):
+    plt.figure()
+    plt.title(label + " Accuracy vs Number of Steps")
+    smoothed_data_list = sig.savgol_filter(data_list, sig_fact_1, sig_fact_2)
+    plt.plot(step_list, smoothed_data_list, label=label)
+    plt.xlabel("Number of batches")
+    plt.ylabel("Accuracy")
+    plt.savefig("{}_accuracy.png".format(label))
+
+def find_num_correct(predictions, label):
+    '''Takes in an array of predictions and of labels. Returns the number of these predicitons that are correct'''
+    predictions = np.argmax((predictions.detach().numpy()), axis=1)  # Both prediction and label 1xnum_genre vectors
+    label = np.argmax((label.detach().numpy()), axis=1)
+    b = (predictions == label)
+    corr_num = int(b.sum())
+    return corr_num
+
 def evaluate(model, val_loader):
     total_corr = 0
     for i, batch in enumerate(val_loader) :
-        feats, label = batch
-        feats = feats.permute(2, 0, 1)
-
+        feats, labels = batch
         predictions = model.forward(feats)
-        predictions = np.argmax((predictions.detach().numpy()), axis=1)
-        b = (predictions == label)
-        corr_num = int(b.sum())
-        total_corr += corr_num
+        total_corr += find_num_correct(predictions, labels)
     return float(total_corr)/len(val_loader.dataset)
 
 
@@ -44,35 +59,40 @@ best_val_acc = 0
 tot_corr = 0
 for counter, epoch in enumerate(range(MaxEpochs)):
     for i, batch in enumerate(train_loader):
-        feats, label = batch
+        feats, labels = batch
         optimizer.zero_grad()
 
         predictions = model.forward(feats)
-        batch_loss = loss_fnc(input=predictions.float(), target=label.float())
+        batch_loss = loss_fnc(input=predictions.float(), target=labels.float())
         batch_loss.backward()
         optimizer.step()
 
-        predictions = np.argmax((predictions.detach().numpy()), axis=1)  #Both prediction and label 1xnum_genre vectors
-        label = np.argmax((label.detach().numpy()), axis=1)
-        b = (predictions == label)
-        corr_num = int(b.sum())
-        tot_corr += corr_num
+        tot_corr += find_num_correct(predictions, labels)
 
-        # #Evaluate and log losses and accuracies for plotting
-        # if (((i + leftover) % eval_every == 0)  and ((i + leftover) != 0)):
-        #     val_acc = evaluate(model, val_loader)
-        #     train_acc = float((tot_corr) / (eval_every * batch_size))
-        #     print("Batch", i, ": Total correct in last", eval_every, "batches is", tot_corr ,
-        #           "out of ", eval_every * batch_size)
-        #     print("Total training accurracy over last batches is ", train_acc)
-        #     print("Total validation accurracy over last batches is ", val_acc, "\n")
-        #
-        #     # Record relevant values
-        #     if len(step_list) == 0:
-        #         step_list.append(0)
-        #     else:
-        #         step_list.append(step_list[-1] + eval_every)
-        #
-        #     train_data_list.append(train_acc)
-        #     val_data_list.append(val_acc)
-        #     tot_corr = 0
+        #Evaluate and log losses and accuracies for plotting
+        if (((i + leftover) % eval_every == 0)  and ((i + leftover) != 0)):  #Leftover makes sure that even if batch size goes over, you graph every eval_evry steps
+            val_acc = evaluate(model, val_loader)
+            train_acc = float((tot_corr) / (eval_every * batch_size))
+            print("Batch", i, ": Total correct in last", eval_every, "batches is", tot_corr ,
+                  "out of ", eval_every * batch_size)
+            print("Total training accurracy over last batches is ", train_acc)
+            print("Total validation accurracy over last batches is ", val_acc, "\n")
+
+            # Record relevant values
+            if len(step_list) == 0:
+                step_list.append(0)
+            else:
+                step_list.append(step_list[-1] + eval_every)
+
+            train_data_list.append(train_acc)
+            val_data_list.append(val_acc)
+            tot_corr = 0
+
+    leftover = ((len(train_loader) % eval_every) * (counter + 1)) % eval_every
+    if val_data_list[-1] > best_val_acc:  # Check model performance and save if best
+        best_val_acc = val_data_list[-1]
+        torch.save(model, "best_model")
+    print("Epoch ", counter, " complete")
+
+    plot_accuracy_vs_stepnum(step_list, train_data_list, "Train", 5, 3)  #Make plots of accuacuers
+    plot_accuracy_vs_stepnum(step_list, val_data_list, "Validation", 11, 5)
