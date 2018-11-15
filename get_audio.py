3d3e5890dcefc2ff47cc2df13cb37849b1aace6d
Robert Adragna <robert.adragna1381@gmail.com>
2018-11-15 17:54:03 -0500

    Added functionality to access server and get metadata for playlists on my account

--- get_audio.py
+++ get_audio.py
@@ -1,5 +1,6 @@
 '''Use spotify API to download 30s previews and song informations'''
 import os
+import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
@@ -11,16 +12,33 @@ def download_mp3_from_url(url, dir):
     r = requests.get(url)
     open(os.path.join(dir, "song.mp3"), 'wb').write(r.content)
 
+existing_songs_path = ""
+#existing_songs = json.loads(open(existing_songs_path).read())  #Dictionary of dictionaries
 
 username = "leafsmaple"
 client_id = "76a157b70b5c4e21b1d1c1e8af629db8"
 client_secret = "621a656e04e94a6d932ad646fef2f485"
 
+#token = util.prompt_for_user_token(username)
 client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+cache_token = client_credentials_manager.get_access_token()
 
-if client_credentials_manager:
-    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+playlist_urls = {'rap': "3LFHhtxgV1bpsJV0SLYfPl",
+                 'rock': "4n5pu9jboAXAKZFEQvQEgY"}
+
+if cache_token:
+    sp = spotipy.Spotify(cache_token)
     sp.trace = False
-    #results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+
+    for plist in playlist_urls:
+        playlist_tracks = sp.user_playlist_tracks(username, playlist_urls[plist], fields="items(track(name,href,album(name,href), external_urls, id))")  #The track we want to deal with has preview link at external_urls field
+        metadata = {}
+        for track in playlist_tracks['items']:
+            metadata[track['track']['id']] = {}
+            metadata[track['track']['id']]["name"] = track['track']['name']
+            metadata[track['track']['id']]["album"] = track['track']['album']
+            metadata[track['track']['id']]["prev"] = track['track']['external_urls']
+            metadata[track['track']['id']]["genre"] = plist
+        
 else:
     print ("Can't get token for", username)
